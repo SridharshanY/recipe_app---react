88121ec5ffa8b209ea3d405eca2965495678e947
SridharshanY <sridharshany.2001@gmail.com>
2024-11-05 10:11:11 +0530

    added .env

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,30 @@
-import React, { useState } from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const Home = () => {
 
-  const [query, setQuery] = useState("")
+  // const [query, setQuery] = useState("")
   const [selectedMeal, setSelectedMeal] = useState("")
+
+  const APP_ID = process.env.REACT_APP_API_ID
+  const APP_KEY = process.env.REACT_APP_API_KEY
+  const query = "pasta"
+
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+  const getData = async() => {
+    try {
+      const {data} = await axios(url)
+      alert(data)
+    } catch (error) {
+      alert(error)
+    }
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+  
 
   return (
     <div>
